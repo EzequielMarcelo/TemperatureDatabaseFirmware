55f57ef68fab2dbfcd7eada26ccd6e63e4a3f4df
Marcelo Gustavo <marcelogustavo05@hotmail.com>
2024-01-03 18:11:38 -0300

    ADDED: Reading temperature in Celsius and plotting on serial. Closes #1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -24,6 +24,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ---
 
+## [Unreleased] - 2023-01-03
+
+### ADDED
+- GetTempCelsius fuction.
+- Configuration of serial communication with baud rate 115200.
+
+---
+
 ## [Unreleased] - 2023-10-15
 
 ### ADDED
--- TemperatureDataBaseFirmware/src/main.cpp
+++ TemperatureDataBaseFirmware/src/main.cpp
@@ -1,11 +1,29 @@
+// ---- Auxiliary libraries ----
 #include <Arduino.h>
 
+//---- Hardware Mapping ----
+#define PIN_TEMPERATURE A0
+
+//---- Scope of functions -----
+float GetTempCelsiusLM35(int adcValue);
+
+//---- Initial Settings ----
 void setup() 
 {
-  
+    Serial.begin(115200);
 }
 
+//---- Main loop ----
 void loop() 
 {
-  
+    int adc = analogRead(PIN_TEMPERATURE);
+    float temperature = GetTempCelsiusLM35(adc);  
+    Serial.println(temperature);                   //debug
+}
+
+//---- Development of functions ----
+float GetTempCelsiusLM35(int adcValue)
+{
+    float voltage = (adcValue * 5.0) / 1023.0;  //Voltage = (ADC * maximum input voltage) / maximum ADC value
+    return voltage / 0.010;                     //The lm35 sensor is linear: 10mV/°C
 }
